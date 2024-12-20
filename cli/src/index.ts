26135ad1b31ec16c9d9bc97f11d70bf7a0517a3b
mcastillo-deskree <m.castillo@deskree.com>
2024-12-20 09:58:54 -0500

    new create node command implemented for testing

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,24 +1,43 @@
 import { Command } from "commander";
+import { createNode } from "./commands/create/node.js";
 import { createProject } from "./commands/create/project.js";
 import { getPackageVersion } from "./services/utils.js";
 
-const program = new Command();
-const version = await getPackageVersion();
-program
-	.name("nanoctl")
-	.version(`${version}`, "-v, --version")
-	.description(`Nanoservice CLI ${version}`)
-	.action(() => {
-		console.log(`Nanoservice CLI: ${version}`);
-	});
+async function main() {
+	try {
+		const program = new Command();
+		const version = await getPackageVersion();
+		program
+			.name("nanoctl")
+			.version(`${version}`, "-v, --version")
+			.description(`Nanoservice CLI ${version}`)
+			.action(() => {
+				console.log(`Nanoservice CLI: ${version}`);
+			});
 
-program
-	.command("create")
-	.description("Create a new nanoservice component")
-	.command("project")
-	.description("Create a new Nano Service Project")
-	.action(() => {
-		createProject();
-	});
+		const create = program.command("create").description("Create a new nanoservice component");
 
-program.parse(process.argv);
+		const project = program
+			.command("project")
+			.description("Create a new Project")
+			.action(() => {
+				createProject();
+			});
+
+		const node = program
+			.command("node")
+			.description("Create a new Node")
+			.action(() => {
+				createNode();
+			});
+
+		create.addCommand(project);
+		create.addCommand(node);
+
+		program.parse(process.argv);
+	} catch (err) {
+		console.log((err as Error).message);
+	}
+}
+
+main();
