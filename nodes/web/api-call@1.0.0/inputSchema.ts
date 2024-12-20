5a0aab0da152103a72a9eec57df5a770fbbbcf1e
mcastillo-deskree <m.castillo@deskree.com>
2024-12-19 19:25:21 -0500

    node base class changed and simlify

---
+++ nodes/web/api-call@1.0.0/inputSchema.ts
@@ -0,0 +1,25 @@
+export const inputSchema = {
+	$schema: "http://json-schema.org/draft-07/schema#",
+	title: "Generated schema for Root",
+	type: "object",
+	properties: {
+		url: {
+			type: "string",
+		},
+		method: {
+			type: "string",
+		},
+		body: {
+			type: "object",
+			properties: {},
+		},
+		headers: {
+			type: "object",
+			properties: {},
+		},
+		responseType: {
+			type: "string",
+		},
+	},
+	required: ["url", "method", "responseType"],
+};
---
+++ nodes/web/api-call@1.0.0/util.ts
@@ -0,0 +1,35 @@
+import type { JsonLikeObject } from "nanoservice-ts-runner";
+
+export const runApiCall = async (
+	url: string,
+	method: string,
+	headers: JsonLikeObject,
+	body: JsonLikeObject,
+	responseType: string,
+): Promise<string | JsonLikeObject> => {
+	const options: {
+		method: string;
+		headers: JsonLikeObject;
+		redirect: "follow";
+		responseType: string;
+		body: string | undefined;
+	} = {
+		method,
+		headers,
+		redirect: "follow",
+		responseType,
+		body: typeof body === "string" ? body : JSON.stringify(body),
+	};
+
+	if (method === "GET") options.body = undefined;
+
+	const response: Response = await fetch(url, options as RequestInit);
+	let parsedResponse: string | JsonLikeObject;
+	if (response.headers.get("content-type")?.includes("application/json")) {
+		parsedResponse = await response.json();
+	} else {
+		parsedResponse = await response.text();
+	}
+	if (!response.ok) throw new Error(parsedResponse as string);
+	return parsedResponse;
+};
