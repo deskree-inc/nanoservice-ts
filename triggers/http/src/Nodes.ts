8fba076a8a718816cd032b35866da1e25c6009a8
mcastillo-deskree <m.castillo@deskree.com>
2025-01-09 16:18:10 -0500

    http trigger is now importing the api-call and if-else nodes

--- triggers/http/src/Nodes.ts
+++ triggers/http/src/Nodes.ts
@@ -1,11 +1,10 @@
-// import ApiCall from "@nanoservice-ts/api-call";
-//import IfElse from "@nanoservice-ts/if-else";
+import ApiCall from "@nanoservice-ts/api-call";
+import IfElse from "@nanoservice-ts/if-else";
 import type Nodes from "./types/Nodes";
-// import { NanoService } from "@nanoservice-ts/runner";
 
 const nodes: Nodes = {
-	// "@nanoservice-ts/api-call": new ApiCall() as NanoService,
-	//"@nanoservice-ts/if-else": new IfElse(),
+	"@nanoservice-ts/api-call": new ApiCall(),
+	"@nanoservice-ts/if-else": new IfElse(),
 };
 
 export default nodes;
