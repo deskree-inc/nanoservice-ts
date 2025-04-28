9d40422a583c50a37cf1aed76011006ecaa8dd74
mcastillo-deskree <m.castillo@deskree.com>
2025-04-28 10:58:00 -0400

    Example: Add image capture, dynamic caption generation, Milvus storage, and multimodal search with UI

--- triggers/http/src/Nodes.ts
+++ triggers/http/src/Nodes.ts
@@ -1,12 +1,14 @@
 import ApiCall from "@nanoservice-ts/api-call";
 import IfElse from "@nanoservice-ts/if-else";
 import type { NodeBase } from "@nanoservice-ts/shared";
+import SaveImageBase64 from "./nodes/examples/save-base64-image";
 
 const nodes: {
 	[key: string]: NodeBase;
 } = {
 	"@nanoservice-ts/api-call": new ApiCall(),
 	"@nanoservice-ts/if-else": new IfElse(),
+	"save-image": new SaveImageBase64(),
 };
 
 export default nodes;
