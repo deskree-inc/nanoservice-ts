7048a4f3a58c6a8b0044fddda187e92caf040acd
ernesto-deskree <ernesto@deskree.com>
2025-05-27 13:24:46 -0500

    APB-483 feat: add /metrics endpoint to expose Prometheus metrics

--- triggers/http/src/opentelemetry_metrics.ts
+++ triggers/http/src/opentelemetry_metrics.ts
@@ -6,8 +6,8 @@ import { MeterProvider } from "@opentelemetry/sdk-metrics";
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 
 // Set up the Prometheus exporter to expose metrics at /metrics on port 9091
-const prometheusExporter = new PrometheusExporter({ port: 9091, endpoint: "/metrics" }, () =>
-	new DefaultLogger().log("Prometheus scrape endpoint: http://localhost:9091/metrics"),
+const prometheusExporter = new PrometheusExporter({}, () =>
+	new DefaultLogger().log("Prometheus scrape endpoint: http://localhost:8080/metrics"),
 );
 
 const resource = Resource.default().merge(
@@ -24,3 +24,8 @@ const meterProvider = new MeterProvider({
 meterProvider.addMetricReader(prometheusExporter);
 
 metrics.setGlobalMeterProvider(meterProvider);
+
+const meter = metrics.getMeter("default");
+const metricsHandler = prometheusExporter.getMetricsRequestHandler.bind(prometheusExporter);
+
+export { meter, metricsHandler };
