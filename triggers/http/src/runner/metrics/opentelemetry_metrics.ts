9f77d9f49a9d1a5cb951f9c01063f027dc235f26
mcastillo-deskree <m.castillo@deskree.com>
2025-05-28 08:15:57 -0400

    impelemented the new metrics for prometheus

--- triggers/http/src/runner/metrics/opentelemetry_metrics.ts
+++ triggers/http/src/runner/metrics/opentelemetry_metrics.ts
@@ -7,7 +7,7 @@ import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic
 
 // Set up the Prometheus exporter to expose metrics at /metrics on port 9091
 const prometheusExporter = new PrometheusExporter({}, () =>
-	new DefaultLogger().log("Prometheus scrape endpoint: http://localhost:8080/metrics"),
+	new DefaultLogger().log("Metrics endpoint: http://localhost:4000/metrics"),
 );
 
 const resource = Resource.default().merge(
