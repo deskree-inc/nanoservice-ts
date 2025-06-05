d764cb6f5c9d64e79afd6ba45d04aeff2c2fa021
ernesto-deskree <ernesto@deskree.com>
2025-06-05 14:11:07 -0500

    Add workflow command to install CLI commands

--- packages/cli/src/commands/install/index.ts
+++ packages/cli/src/commands/install/index.ts
@@ -1,5 +1,6 @@
 import { program } from "../../services/commander.js";
 
 import node from "./node.js";
+import workflow from "./workflow.js";
 // Nodes commands
-program.command("install").description("Install commands").addCommand(node);
+program.command("install").description("Install commands").addCommand(node).addCommand(workflow);
